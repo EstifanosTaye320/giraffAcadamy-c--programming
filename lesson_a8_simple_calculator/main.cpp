188d5f0dd251672521effe122cc300ef51b41189
Estifanos Taye <estifanostaye11@gmail.com>
2024-03-14 17:46:35 +0300

    lesson 8 simple calculator

---
+++ lesson_a8_simple_calculator/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double firstNumber, secondNumber;
+
+  cout << "Enter the first Number: ";
+  cin >> firstNumber;
+
+  cout << "Enter the second Number: ";
+  cin >> secondNumber;
+
+
+  cout << firstNumber << " ";
+  cout << "+" << " ";
+  cout << secondNumber << " ";
+  cout << "=" << " ";
+  cout << firstNumber + secondNumber;
+  return 0;
+}
