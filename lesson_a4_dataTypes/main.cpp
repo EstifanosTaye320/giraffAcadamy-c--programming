96f3fde3bc654ca790ef9c3999613506af4392f6
Estifanos Taye <estifanostaye11@gmail.com>
2024-03-14 16:12:38 +0300

    lesson 4 datatypes

---
+++ lesson_a4_dataTypes/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char myLetter = 'A';
+  string phrase = "nice";
+  int age = 50;
+  double gpa = 2.4333;
+  float lessDP = 3.22;
+  bool isMale = true;
+
+  cout << myLetter << endl;
+
+  return 0;
+}
