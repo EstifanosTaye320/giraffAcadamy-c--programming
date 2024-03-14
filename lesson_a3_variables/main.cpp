f23c3b9674ee8f36f45dcddf09ffc799211a75d6
Estifanos Taye <estifanostaye11@gmail.com>
2024-03-14 16:01:28 +0300

    lesson 3 variables

---
+++ lesson_a3_variables/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string characterName = "John";
+  int characterAge;
+  
+  characterAge = 35;
+  cout << "There was once a man named " << characterName << endl;
+  cout << "He was " << characterAge << " years old"<< endl;
+  cout << "He really liked the name " << characterName << endl;
+  cout << "But didn't like being " << characterAge << endl;
+}
